27a2e859441e60bc561f469b231434accffd44f8
Rahul Sahu <rahul.et2016@gmail.com>
2023-02-07 07:36:50 +0530

    Initial commit

---
+++ src/main/java/com/rahulpetclinic/rahulpetclinic/RahulPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.rahulpetclinic.rahulpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RahulPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RahulPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahulpetclinic/rahulpetclinic/RahulPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahulpetclinic.rahulpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RahulPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
